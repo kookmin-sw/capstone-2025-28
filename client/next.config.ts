9639aece36e6f05cf28c68d84a39ed98ca9d90e6
SKrns <rnsk3626@gmail.com>
2025-04-10 09:35:24 +0900

    set environmet config

--- client/next.config.ts
+++ client/next.config.ts
@@ -3,6 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   transpilePackages: ['three'],
+  experimental: {
+    allowedDevOrigins: [
+      'http://ec2-13-125-170-246.ap-northeast-2.compute.amazonaws.com:3001',
+      'https://ec2-13-125-170-246.ap-northeast-2.compute.amazonaws.com:3001',
+    ],
+  },
 };
 
 export default nextConfig;
